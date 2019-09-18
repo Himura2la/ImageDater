ca2a1ab233ab6fcd13cdd396ca2806217cd37655
Vladislav Glagolev (DevExpress) <vladislav.glagolev@devexpress.com>
2019-09-18 16:44:16 +0300

    Fix orientation and relative size

--- dater2.py
+++ dater2.py
@@ -12,7 +12,6 @@ import piexif # pip install piexif
 размер_шрифта = 100
 отступ_слева = 20
 отступ_снизу = 20
-шрифт = ImageFont.truetype('%WINDIR%\\Fonts\\arial.ttf', размер_шрифта)
 цвет_шрифта = (255, 255, 255)
 прозрачность_подложки = 30  # Процентов
 цвет_подложки = (0, 0, 0, round(прозрачность_подложки/100*255))
@@ -30,10 +29,13 @@ class Приложение(ttk.Frame):
         self.master = master
         self.master.title("Проставлялка дат")
         self.даты = {}
+        self.exif = {}
 
         for картинка in пути_к_картинкам:
             self.строка = ttk.Frame(self)
-            self.даты[картинка] = tkinter.StringVar(self.строка, value=прочитать_дату(картинка))
+            self.exif[картинка] = прочитать_EXIF(картинка)
+            дата = self.exif[картинка]["дата"] if self.exif[картинка] else "пусто"
+            self.даты[картинка] = tkinter.StringVar(self.строка, value=дата)
 
             self.надпись_файл = tkinter.Label(self.строка, text=картинка)
             self.надпись_файл.pack(side='left')
@@ -49,32 +51,67 @@ class Приложение(ttk.Frame):
     def проставить_всем(self):
         for картинка, поле_даты in self.даты.items():
             дата = поле_даты.get()
-            проставить_дату(картинка, дата)
+            проставить_дату(картинка, дата, self.exif[картинка]["ориентация"])
         self.master.destroy()
         return
 
 
-def прочитать_дату(путь_к_файлу):
+def прочитать_EXIF(путь_к_файлу):
     картинка = Image.open(путь_к_файлу).convert('RGB')
     try:
         exif = piexif.load(картинка.info['exif'])
         дата_съемки = exif['0th'][piexif.ImageIFD.DateTime].decode('utf-8')
         дата, время = дата_съемки.split(' ', 1)
         год, месяц, число = дата.split(':', 3)
-        return f"{число}.{месяц}.{год[2:]} {время}"
+        ориентация = exif["0th"][piexif.ImageIFD.Orientation]
+        return {
+            "дата": f"{число}.{месяц}.{год[2:]} {время}",
+            "ориентация": ориентация
+        }
     except KeyError:
-        return "бездато"
+        return None
 
 
-def проставить_дату(путь_к_файлу, дата_съемки):
+def проставить_дату(путь_к_файлу, дата_съемки, ориентация):
     картинка = Image.open(путь_к_файлу).convert('RGB')
+
+    if ориентация == 2:
+        картинка = картинка.transpose(Image.FLIP_LEFT_RIGHT)
+    elif ориентация == 3:
+        картинка = картинка.rotate(180)
+    elif ориентация == 4:
+        картинка = картинка.rotate(180).transpose(Image.FLIP_LEFT_RIGHT)
+    elif ориентация == 5:
+        картинка = картинка.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+    elif ориентация == 6:
+        картинка = картинка.rotate(-90, expand=True)
+    elif ориентация == 7:
+        картинка = картинка.rotate(90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+    elif ориентация == 8:
+        картинка = картинка.rotate(90, expand=True)
+
     ширина, высота = картинка.size
+    разрешение = (ширина**2 + высота**2)**0.5
+    относительный_размер_шрифта = int(размер_шрифта * разрешение / 5000)
+    шрифт = ImageFont.truetype('%WINDIR%\\Fonts\\arial.ttf', относительный_размер_шрифта)
+
+    относительный_отступ_снизу = int(отступ_снизу * разрешение / 5000)
+    относительный_отступ_слева = int(отступ_слева * разрешение / 5000)
+    
     draw = ImageDraw.Draw(картинка, 'RGBA')
 
     text_width, text_height = draw.textsize(дата_съемки, шрифт)
-    draw.rectangle([отступ_снизу, высота - text_height - отступ_снизу, text_width + отступ_слева, высота - отступ_снизу], цвет_подложки)
+    draw.rectangle([относительный_отступ_снизу,
+                    высота - text_height - относительный_отступ_снизу,
+                    text_width + относительный_отступ_слева,
+                    высота - относительный_отступ_снизу - 1],
+                   цвет_подложки)
 
-    draw.text((отступ_слева, высота - text_height - round(размер_шрифта / 10) - отступ_снизу), дата_съемки, цвет_шрифта, шрифт)
+    draw.text((относительный_отступ_слева,
+               высота - text_height - round(относительный_размер_шрифта / 10) - относительный_отступ_снизу),
+              дата_съемки,
+              цвет_шрифта,
+              шрифт)
 
     folder, filename = os.path.split(путь_к_файлу)
     картинка.save(os.path.join(folder, префикс_имён_файлов + filename))
